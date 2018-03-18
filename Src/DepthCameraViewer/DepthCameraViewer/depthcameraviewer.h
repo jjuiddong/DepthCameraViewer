2fe25454c04762676c5c38f80a2713cceb1650c9
lee jaejung <jjuiddong@gmail.com>
2018-03-18 12:43:54 +0900

    update depth camera viewer

---
+++ Src/DepthCameraViewer/DepthCameraViewer/depthcameraviewer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+class c3DView;
+
+class cViewer : public framework::cGameMain2
+{
+public:
+	cViewer();
+	virtual ~cViewer();
+
+	virtual bool OnInit() override;
+	virtual void OnUpdate(const float deltaSeconds) override;
+	virtual void OnRender(const float deltaSeconds) override;
+	virtual void OnEventProc(const sf::Event &evt) override;
+
+
+public:
+	c3DView *m_3dView;
+};
